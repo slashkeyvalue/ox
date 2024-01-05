6d8bce19afe10a5e1f7d40049553178e5a928381
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-06 00:07:43 +1100

    make db pretty for luke

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,6 @@
-import { getConnection } from "./pool";
+import { getConnection } from './pool';
 
-type QueryTypes = "query" | "single" | "scalar";
+type QueryTypes = 'query' | 'single' | 'scalar';
 type QueryValues = (string | number)[];
 type SqlColumn<T> = T extends any[] ? never : T;
 type SqlRow<T> = T extends Dict<any> ? T : never;
@@ -8,9 +8,9 @@ type SqlRows<T> = T extends Dict<any>[] ? T : never;
 
 function parseResponse(type: QueryTypes, resp: any) {
   switch (type) {
-    case "single":
+    case 'single':
       return resp[0];
-    case "scalar":
+    case 'scalar':
       for (const key in resp[0]) return resp[0][key];
     default:
       return resp;
@@ -18,34 +18,34 @@ function parseResponse(type: QueryTypes, resp: any) {
 }
 
 interface Db {
-  query<T>(query: string, values?: QueryValues, type?: "query"): Promise<SqlRows<T>>
-  query<T>(query: string, values?: QueryValues, type?: "single"): Promise<SqlRow<T> | void>
-  query<T>(query: string, values?: QueryValues, type?: "scalar"): Promise<SqlColumn<T> | void>
-  execute<T>(query: string, values?: QueryValues, type?: "query"): Promise<SqlRows<T>>
-  execute<T>(query: string, values?: QueryValues, type?: "single"): Promise<SqlRow<T> | void>
-  execute<T>(query: string, values?: QueryValues, type?: "scalar"): Promise<SqlColumn<T> | void>
-  single<T>(query: string, values?: QueryValues): Promise<SqlRow<T> | void>
-  scalar<T>(query: string, values?: QueryValues): Promise<SqlColumn<T> | void>
+  query<T>(query: string, values?: QueryValues, type?: 'query'): Promise<SqlRows<T>>;
+  query<T>(query: string, values?: QueryValues, type?: 'single'): Promise<SqlRow<T> | void>;
+  query<T>(query: string, values?: QueryValues, type?: 'scalar'): Promise<SqlColumn<T> | void>;
+  execute<T>(query: string, values?: QueryValues, type?: 'query'): Promise<SqlRows<T>>;
+  execute<T>(query: string, values?: QueryValues, type?: 'single'): Promise<SqlRow<T> | void>;
+  execute<T>(query: string, values?: QueryValues, type?: 'scalar'): Promise<SqlColumn<T> | void>;
+  single<T>(query: string, values?: QueryValues): Promise<SqlRow<T> | void>;
+  scalar<T>(query: string, values?: QueryValues): Promise<SqlColumn<T> | void>;
 }
 
 export const db: Db = {
-  async query(query, values, type = "query") {
+  async query(query, values, type = 'query') {
     using conn = await getConnection();
 
     return parseResponse(type, await conn.query(query, values));
   },
 
-  async execute(query, values, type = "query") {
+  async execute(query, values, type = 'query') {
     using conn = await getConnection();
 
     return parseResponse(type, await conn.execute(query, values));
   },
 
   async single(query, values?) {
-    return db.query(query, values, "single");
+    return db.query(query, values, 'single');
   },
 
   async scalar(query, values?) {
-    return db.query(query, values, "scalar");
-  }
-}
+    return db.query(query, values, 'scalar');
+  },
+};
