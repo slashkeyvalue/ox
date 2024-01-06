ce08d9c61df01317f222b9197c9ea75db3a48699
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-06 16:40:53 +1100

    resource config

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,7 @@
+import { PLATE_FORMAT } from 'config';
+
 export * from '../common';
+
+for (let i = 0; i < GetNumberOfVehicleNumberPlates(); i++) {
+  SetDefaultVehicleNumberPlateTextPattern(i, PLATE_FORMAT);
+}
---
+++ common/config.ts
@@ -0,0 +1,4 @@
+export const SV_LAN = GetConvarInt('sv_lan', 0) === 1;
+export const DEBUG = SV_LAN || GetConvarInt('ox:debug', 0) === 1;
+export const CHARACTER_SLOTS = GetConvarInt('ox:characterSlots', 1);
+export const PLATE_FORMAT = GetConvar('ox:plateFormat', '........').toUpperCase();
