03aa204bde906f0aa8275aff4859eb87b5ead7a4
Linden <65407488+thelindat@users.noreply.github.com>
2024-01-10 09:53:48 +1100

    add npwd support

--- server/config.ts
+++ server/config.ts
@@ -5,14 +5,3 @@ export const PRIMARY_IDENTIFIER = GetConvar('ox:primaryIdentifier', 'license2');
 // Set ox_inventory convars
 SetConvarReplicated('inventory:framework', 'ox');
 SetConvarReplicated('inventory:trimplate ', 'false');
-
-// Set npwd convars
-SetConvar('npwd:useResourceIntegration', 'true');
-SetConvar(
-  'npwd:database',
-  JSON.stringify({
-    playerTable: 'characters',
-    identifierColumn: 'charId',
-    phoneNumberColumn: 'phoneNumber',
-  })
-);
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 export * from '../common';
+import './bridge';
 import 'player';
 import 'utils';
 import 'commands';
